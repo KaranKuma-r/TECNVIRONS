4f398c96bca7d174f1b8467a396b55b528064ab5
Karan Kumar <kk5214662@gmail.com>
2026-03-09 00:22:52 +0530

    Add cors

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
-
+app.use(cors());  
 app.use(express.json());
 
 
