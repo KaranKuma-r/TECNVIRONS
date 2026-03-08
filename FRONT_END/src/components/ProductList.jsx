49b6fa42bbe3ab1b0a987489917b01eff0666db5
Karan Kumar <kk5214662@gmail.com>
2026-03-09 00:14:28 +0530

    Implement ProductList component with responsive grid layout

---
+++ FRONT_END/src/components/ProductList.jsx
@@ -0,0 +1,17 @@
+import ProductCard from "./ProductCard";
+
+function ProductList({ products }) {
+
+  return (
+    <div className="product-grid">
+
+      {products.map((p) => (
+        <ProductCard key={p.id} product={p}/>
+      ))}
+
+    </div>
+  );
+
+}
+
+export default ProductList;
